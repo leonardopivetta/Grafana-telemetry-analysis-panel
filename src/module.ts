c5b6319fba82cbae7fa3917153c569907746d795
leonardopivetta <leonardo.pivetta101@gmail.com>
2021-11-06 19:24:30 +0100

    added dynamic menu generation

--- src/module.ts
+++ src/module.ts
@@ -1,18 +1,28 @@
 import { PanelPlugin } from '@grafana/data';
 import { CustomPanelsTelemetryAnalysis, CustomPanelTelemetryAnalysisProps } from './CustomPanelsTelemetryAnalysis';
 import { CustomPanelTypes } from './types';
+import _ from "lodash";
 
 export const plugin = new PanelPlugin<CustomPanelTelemetryAnalysisProps>(CustomPanelsTelemetryAnalysis).setPanelOptions(builder => {
+
+  var options: {
+    value: CustomPanelTypes;
+    label: string;
+  }[] = [];
+  
+  _.forIn(CustomPanelTypes, (key, value)=>{
+    options.push({
+      value: key,
+      label: value
+    })
+  }); 
+
   return builder.addRadio({
     name: 'Type of graph',
     path: 'selector',
     defaultValue: CustomPanelTypes.Gauge,
     settings: {
-      options: [
-        {value: CustomPanelTypes.Gauge, label: 'Gauge'},
-        {value: CustomPanelTypes.Map, label: 'Map'},
-        {value: CustomPanelTypes.SteeringWheel, label: 'Steering Wheel'}
-      ]
+      options: options
     }
   });
 });
